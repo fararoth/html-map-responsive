b1f7be7ae067aaeeb697038cca38e9097076e5c4
Luis Farias <luisfariasmusicista@gmail.com>
2022-11-04 15:58:35 -0400

    Adding first responsive features

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 document.addEventListener("DOMContentLoaded", ()=>{
     var mapArea1 = document.getElementById("branch-1-area");
     var cardInfo1 = document.getElementsByClassName("card-info-1")[0];
+    var upBranch = document.getElementsByClassName("branch-1-up")[0];
+    var downBranch = document.getElementsByClassName("branch-1-down")[0];
 
     mapArea1.addEventListener("mouseover", ()=>{
-        cardInfo1 .style.display = "block";
+        cardInfo1.style.display = "block";
     })
 
     mapArea1.addEventListener("mouseout", ()=>{
-        cardInfo1 .style.display = "none";
+        cardInfo1.style.display = "none";
     })
+
+    imageMapResize();
+    
 })
 
+$(document).ready(function() {
+        $('#image-map').maphilight();
+});
