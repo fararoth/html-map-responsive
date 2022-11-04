e58200cf10a1c2d48987cae3b18b6419410ff38c
Luis Farias <luisfariasmusicista@gmail.com>
2022-11-04 12:20:32 -0400

    Early structures and early functionality

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    var mapArea1 = document.getElementById("branch-1-area");
+    var cardInfo1 = document.getElementsByClassName("card-info-1")[0];
+
+    mapArea1.addEventListener("mouseover", ()=>{
+        cardInfo1 .style.display = "block";
+    })
+
+    mapArea1.addEventListener("mouseout", ()=>{
+        cardInfo1 .style.display = "none";
+    })
+})
+
