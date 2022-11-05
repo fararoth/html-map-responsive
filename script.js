efa6bf2782b9410064f4e220af2ef93c5587ac3f
Luis Farias <luisfariasmusicista@gmail.com>
2022-11-05 17:50:12 -0400

    small things

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 document.addEventListener("DOMContentLoaded", ()=>{
     var mapArea1 = document.getElementById("branch-1-area");
     var cardInfo1 = document.getElementsByClassName("card-info-1")[0];
-    var upBranch = document.getElementsByClassName("branch-1-up")[0];
-    var downBranch = document.getElementsByClassName("branch-1-down")[0];
 
     mapArea1.addEventListener("mouseover", ()=>{
         cardInfo1.style.display = "block";
